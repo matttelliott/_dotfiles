e03c8152f1ad23b7fcd4f47c027995697b08f079
Matt T Elliott <matt@matttelliott.com>
2026-01-14 23:17:21 -0600

    Claude auto commit - tools/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh

---
+++ tools/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
@@ -0,0 +1,5 @@
+if [[ -f /opt/homebrew/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh ]]; then
+  source /opt/homebrew/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+elif [[ -f /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh ]]; then
+  source /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+fi
