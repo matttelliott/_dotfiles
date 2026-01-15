f5cbfa2d95f1a84edd432e908290f8d840fa4589
Matt T Elliott <matt@matttelliott.com>
2026-01-15 01:40:29 -0600

    Claude auto commit - tools/node/node.zsh

---
+++ tools/node/node.zsh
@@ -0,0 +1,13 @@
+# Lazy-load nvm (only loads when you use nvm/node/npm/npx)
+export NVM_DIR="$HOME/.nvm"
+
+_load_nvm() {
+  unset -f nvm node npm npx
+  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+  [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"
+}
+
+nvm() { _load_nvm && nvm "$@"; }
+node() { _load_nvm && node "$@"; }
+npm() { _load_nvm && npm "$@"; }
+npx() { _load_nvm && npx "$@"; }
