13647e8c163f1d026b14b74baa7afdca5b39cbe8
Matt T Elliott <matt@matttelliott.com>
2026-01-14 23:17:24 -0600

    Claude auto commit - tools/zsh-autosuggestions/zsh-autosuggestions.zsh

---
+++ tools/zsh-autosuggestions/zsh-autosuggestions.zsh
@@ -0,0 +1,5 @@
+if [[ -f /opt/homebrew/share/zsh-autosuggestions/zsh-autosuggestions.zsh ]]; then
+  source /opt/homebrew/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+elif [[ -f /usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh ]]; then
+  source /usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+fi
