58eaa315c16452bf85eb2d55100cc8bba816d75e
Matt T Elliott <matt@matttelliott.com>
2026-01-16 02:18:12 -0600

    wip

--- tools/neovim/nvim/lua/custom/plugins/init.lua
+++ tools/neovim/nvim/lua/custom/plugins/init.lua
@@ -2,4 +2,9 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  { 'Mofiqul/dracula.nvim' },
+  { 'ellisonleao/gruvbox.nvim' },
+  { 'catppuccin/nvim', name = 'catppuccin' },
+  { 'folke/tokyonight.nvim' },
+}
