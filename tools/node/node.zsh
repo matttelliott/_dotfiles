350002ca14af9eaf08c44d938492e0c2e443bc82
Matt T Elliott <matt@matttelliott.com>
2026-01-18 16:17:09 -0600

    Claude auto commit - tools/node/node.zsh

--- tools/node/node.zsh
+++ tools/node/node.zsh
@@ -1,19 +1,8 @@
-# Fast nvm loading
+# nvm
 export NVM_DIR="$HOME/.nvm"
 
-# Load nvm with --no-use (skips slow node activation)
 if [ -s "$NVM_DIR/nvm.sh" ]; then
-  \. "$NVM_DIR/nvm.sh" --no-use
+  \. "$NVM_DIR/nvm.sh"
 elif [ -s "/opt/homebrew/opt/nvm/nvm.sh" ]; then
-  \. "/opt/homebrew/opt/nvm/nvm.sh" --no-use
-fi
-
-# Manually add default node to PATH (much faster than nvm use)
-if [ -d "$NVM_DIR/versions/node" ]; then
-  NODE_DEFAULT="$NVM_DIR/alias/default"
-  if [ -f "$NODE_DEFAULT" ]; then
-    NODE_VERSION=$(cat "$NODE_DEFAULT")
-    NODE_PATH="$NVM_DIR/versions/node/v${NODE_VERSION#v}/bin"
-    [ -d "$NODE_PATH" ] && export PATH="$NODE_PATH:$PATH"
-  fi
+  \. "/opt/homebrew/opt/nvm/nvm.sh"
 fi
