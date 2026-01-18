5638b243b055eeb3c756cb1979454c9ccd49df7a
matt <matt@macbookair.home.lan>
2026-01-17 19:03:19 -0600

    Claude auto commit - tools/wezterm/wezterm.lua

--- tools/wezterm/wezterm.lua
+++ tools/wezterm/wezterm.lua
@@ -5,6 +5,7 @@ config.font = wezterm.font("DejaVuSansM Nerd Font Mono")
 config.font_size = 18
 config.color_scheme = "tokyonight"
 config.enable_tab_bar = false
+config.window_close_confirmation = "NeverPrompt"
 config.default_prog = { "/bin/zsh", "-c", "tmux a || tmux" }
 config.keys = {
 	{ key = "Enter", mods = "CMD", action = wezterm.action.ToggleFullScreen },
