9025d4fc82f48c461878c4e6c40d5fd1ee03b918
Matt T Elliott <matt@matttelliott.com>
2026-02-20 05:43:34 -0600

    Claude auto commit - tools/neovim/nvim/lua/custom/plugins/diffview.lua

--- tools/neovim/nvim/lua/custom/plugins/diffview.lua
+++ tools/neovim/nvim/lua/custom/plugins/diffview.lua
@@ -1,5 +1,4 @@
 -- diffview.nvim - Git diff/history viewer
--- TODO: Evaluate and configure further
 return {
   'sindrets/diffview.nvim',
   cmd = { 'DiffviewOpen', 'DiffviewFileHistory' },
@@ -7,6 +6,41 @@ return {
     { '<leader>gd', '<cmd>DiffviewOpen<cr>', desc = '[G]it [D]iff view' },
     { '<leader>gh', '<cmd>DiffviewFileHistory %<cr>', desc = '[G]it file [H]istory' },
     { '<leader>gH', '<cmd>DiffviewFileHistory<cr>', desc = '[G]it repo [H]istory' },
+    { '<leader>gp', '<cmd>DiffviewOpen origin/main...HEAD<cr>', desc = '[G]it [P]R review vs origin/main' },
+    {
+      '<leader>gb',
+      function()
+        require('telescope.builtin').git_branches {
+          prompt_title = 'Diff against branch',
+          attach_mappings = function(_, map)
+            map('i', '<CR>', function(prompt_bufnr)
+              local entry = require('telescope.actions.state').get_selected_entry(prompt_bufnr)
+              require('telescope.actions').close(prompt_bufnr)
+              vim.cmd('DiffviewOpen ' .. entry.value .. '...HEAD')
+            end)
+            return true
+          end,
+        }
+      end,
+      desc = '[G]it diff against [B]ranch',
+    },
+    {
+      '<leader>gc',
+      function()
+        require('telescope.builtin').git_commits {
+          prompt_title = 'Diff against commit',
+          attach_mappings = function(_, map)
+            map('i', '<CR>', function(prompt_bufnr)
+              local entry = require('telescope.actions.state').get_selected_entry(prompt_bufnr)
+              require('telescope.actions').close(prompt_bufnr)
+              vim.cmd('DiffviewOpen ' .. entry.value)
+            end)
+            return true
+          end,
+        }
+      end,
+      desc = '[G]it diff against [C]ommit',
+    },
   },
   opts = {
     enhanced_diff_hl = true,
